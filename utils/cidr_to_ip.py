f7d4ce7975c8f1892d4f2d1807043e89158cdd2d
JF <justinlf@gmail.com>
2020-07-10 08:24:21 -0400

    Adds CIDR to IP converter

---
+++ utils/cidr_to_ip.py
@@ -0,0 +1,20 @@
+import argparse
+import ipaddress
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument('--infile', help="File with IPs in CIDR format", required=True)
+parser.add_argument('--outfile', help="Where to save the results", required=True)
+args = parser.parse_args()
+
+cidrList = list()
+ipList = list()
+
+cidrList = [line.strip('\n') for line in open(args.infile, 'r')]
+
+for c in cidrList:
+  for ip in ipaddress.IPv4Network(c):
+    ipList.append(str(ip))
+
+f = open(args.outfile, 'a')
+for ip in ipList:
+  f.write("{}\n".format(ip))
